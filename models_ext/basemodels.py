be53b7f9507f184c8c6794a64976537d46008658
Brightcells <brightcells@gmail.com>
2018-01-13 01:13:48 +0800

    :sparkles: Add AbstractModel

--- models_ext/basemodels.py
+++ models_ext/basemodels.py
@@ -4,16 +4,18 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-class BaseModelMixin(models.Model):
-    status = models.BooleanField(_(u'status'), default=True, help_text=_(u'Status'), db_index=True)
-    created_at = models.DateTimeField(_(u'created_at'), auto_now_add=True, editable=True, help_text=_(u'Create Time'))
-    updated_at = models.DateTimeField(_(u'updated_at'), auto_now=True, editable=True, help_text=_(u'Update Time'))
-
+class AbstractModel(object):
     class Meta:
         abstract = True
 
 
-class SexModelMixin(models.Model):
+class BaseModelMixin(AbstractModel, models.Model):
+    status = models.BooleanField(_(u'status'), default=True, help_text=_(u'Status'), db_index=True)
+    created_at = models.DateTimeField(_(u'created_at'), auto_now_add=True, editable=True, help_text=_(u'Create Time'))
+    updated_at = models.DateTimeField(_(u'updated_at'), auto_now=True, editable=True, help_text=_(u'Update Time'))
+
+
+class SexModelMixin(AbstractModel, models.Model):
     MALE = 1
     FEMALE = 0
 
@@ -23,6 +25,3 @@ class SexModelMixin(models.Model):
     )
 
     sex = models.BooleanField(_(u'sex'), choices=SEX_TUPLE, default=MALE, help_text=_(u'Sex'), db_index=True)
-
-    class Meta:
-        abstract = True
