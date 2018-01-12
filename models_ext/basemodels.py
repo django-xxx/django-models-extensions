fb0e4b6faab7abb5e5c6456a705c096ab6c15956
Brightcells <brightcells@gmail.com>
2018-01-13 01:30:20 +0800

    :bug: Remove AbstractModel

--- models_ext/basemodels.py
+++ models_ext/basemodels.py
@@ -4,18 +4,16 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-class AbstractModel(object):
-    class Meta:
-        abstract = True
-
-
-class BaseModelMixin(AbstractModel, models.Model):
+class BaseModelMixin(models.Model):
     status = models.BooleanField(_(u'status'), default=True, help_text=_(u'Status'), db_index=True)
     created_at = models.DateTimeField(_(u'created_at'), auto_now_add=True, editable=True, help_text=_(u'Create Time'))
     updated_at = models.DateTimeField(_(u'updated_at'), auto_now=True, editable=True, help_text=_(u'Update Time'))
 
+    class Meta:
+        abstract = True
 
-class SexModelMixin(AbstractModel, models.Model):
+
+class SexModelMixin(models.Model):
     MALE = 1
     FEMALE = 0
 
@@ -25,3 +23,6 @@ class SexModelMixin(AbstractModel, models.Model):
     )
 
     sex = models.BooleanField(_(u'sex'), choices=SEX_TUPLE, default=MALE, help_text=_(u'Sex'), db_index=True)
+
+    class Meta:
+        abstract = True
