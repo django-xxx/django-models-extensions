018b9202065c0341bf058883759631fabb85f8dc
Brightcells <brightcells@gmail.com>
2018-11-07 01:44:01 +0800

    :sparkles: Support get_or_create_ext

--- django_models_ext/__init__.py
+++ django_models_ext/__init__.py
@@ -3,3 +3,4 @@
 from django_models_ext.basemodels import BaseModelMixin, SexModelMixin
 from django_models_ext.provincemodels import ProvinceModelMixin, ProvinceShortModelMixin
 from django_models_ext.fileext import upload_path, upload_file_path, upload_file_store_path, upload_file_url
+from django_models_ext.queryext import get_or_create_ext
