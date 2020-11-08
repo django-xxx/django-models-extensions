10d6d5d9aeb0d8c510e046a095cf0562e2cbb9a8
huangqimin001 <huangqimin001@ke.com>
2020-11-09 03:37:35 +0800

    :art: Remove db_index=True

--- django_models_ext/basemodels.py
+++ django_models_ext/basemodels.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class BaseModelMixin(models.Model):
-    status = models.BooleanField(_(u'status'), default=True, help_text=_(u'Status'), db_index=True)
+    status = models.BooleanField(_(u'status'), default=True, help_text=_(u'Status'))
     created_at = models.DateTimeField(_(u'created_at'), auto_now_add=True, editable=True, help_text=_(u'Create Time'))
     updated_at = models.DateTimeField(_(u'updated_at'), auto_now=True, editable=True, help_text=_(u'Update Time'))
 
@@ -24,7 +24,7 @@ class SexModelMixin(models.Model):
         (FEMALE, u'女'),
     )
 
-    sex = models.IntegerField(_(u'sex'), choices=SEX_TUPLE, default=UNKNOWN, help_text=_(u'Sex'), db_index=True)
+    sex = models.IntegerField(_(u'sex'), choices=SEX_TUPLE, default=UNKNOWN, help_text=_(u'Sex'))
 
     class Meta:
         abstract = True
